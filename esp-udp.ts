96050e5a0460df11583dae8dd15bd5515c89593f
dastu08 <37212582+dastu08@users.noreply.github.com>
2021-10-22 21:00:28 +0200

    Provide get/set functions
    get/set functions mask sending JSON objects via UDP.

--- esp-udp.ts
+++ esp-udp.ts
@@ -1,7 +1,5 @@
 import * as udpclient from "./udp-client"
 
-let listenContinousFlag: boolean = false;
-let heartbeat_last: string = "never";
 let lift_message_handle: (level: string, message: string) => void;
 
 type espQuantity = {
@@ -16,28 +14,16 @@ type udpObjReceive = {
     quantity?: espQuantity[];
 }
 
-// turn off continuous listening
-export function listenOff() {
-    listenContinousFlag = false;
-}
-
-// turn on continuous listening
-export function listenOn() {
-    listenContinousFlag = true;
-}
-
-// get the time value of the last heartbeat
-export function getLastHeartbeat(): string {
-    return heartbeat_last;
-}
-
-export function send(msg: string) {
-    udpclient.send(msg);
+type udpObjSend = {
+    type: string;
+    quantity?: string[];
+    name?: string;
+    value?: string | number;
 }
 
 export function start(ipaddr: string, port: number, callback: (level: string, message: string) => void) {
-    // udpclient.init(ipaddr, port);
-    
+    udpclient.init(ipaddr, port);
+
     // set the callback function that is called when an incoming message is
     // received
     lift_message_handle = callback;
@@ -71,12 +57,12 @@ function udp_message_handle(msg: string) {
                         replyText += `pressure = ${element.value} ${element.unit}. `;
                     }
                 });
-                lift_message_handle("info", replyText);
+                lift_message_handle("response", replyText);
                 break;
 
             // measurement is send periodically w/o a request
             case "measurement":
-                if (listenContinousFlag && obj.hasOwnProperty("time") && obj.hasOwnProperty("quantity")) {
+                if (obj.hasOwnProperty("time") && obj.hasOwnProperty("quantity")) {
                     replyText += `${obj.time.slice(11)} : `;
                     // loop through the list of quantity
                     obj.quantity.forEach(element => {
@@ -87,14 +73,14 @@ function udp_message_handle(msg: string) {
                             replyText += `${element.value} ${element.unit}. `;
                         }
                     });
-                    lift_message_handle("info", replyText);
+                    lift_message_handle("measurement", replyText);
                 }
                 break;
 
             // remember last hearbeat time
             case "heartbeat":
-                heartbeat_last = obj.time;
-                lift_message_handle("debug", `Got heartbeat ${heartbeat_last.slice(11)}`);
+                // heartbeat_last = obj.time;
+                lift_message_handle("heartbeat", `${obj.time.slice(11)}`);
                 break;
 
             case "error":
@@ -111,3 +97,47 @@ function udp_message_handle(msg: string) {
     }
 }
 
+export function get(quantity: string) {
+    let res: udpObjSend = { type: "get" };
+
+    switch (quantity) {
+        case "temperature":
+            res.quantity = ["temperature"];
+            break;
+
+        case "pressure":
+            res.quantity = ["pressure"];
+            break;
+
+        case "all":
+            res.quantity = ["temperature", "pressure"];
+            break;
+
+        default:
+            break;
+    }
+    udpclient.send(JSON.stringify(res));
+}
+
+export function set(quantity: string, value: string | number) {
+    let res: udpObjSend = { type: "set" };
+    switch (quantity) {
+        case "heartbeat":
+            res.name = "heartbeat";
+            res.value = value;
+            break;
+
+        case "heartbeat_interval":
+            res.name = "heartbeat_interval";
+            res.value = value;
+            break;
+
+        case "listen_interval":
+            res.name = "measurement_interval";
+            res.value = value;
+        default:
+            console.error("Quantity not found!");
+            break;
+    }
+    udpclient.send(JSON.stringify(res));
+}
