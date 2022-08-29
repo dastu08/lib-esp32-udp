fc4f5098d67767171007c322af73a312047bcd7b
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-08-29 16:43:33 +0200

    Update logging, argument type
    Print the number of bytes and words of sent and received messages.
    
    Argument a buffer instead of a string when passing the incoming message
    to the callback.

--- udp-client.ts
+++ udp-client.ts
@@ -1,7 +1,6 @@
 // Library to provide the UDP client in NodeJS
 
 import exp = require("constants");
-import { connected } from "process";
 
 // UDP setup from NodeJS
 const dgram = require('dgram');
@@ -10,7 +9,7 @@ const udp = dgram.createSocket('udp4');
 // UDP client configuration
 var esp_udpport: number = -1;
 var esp_ipaddr: string = "";
-var log_debug = false;
+var log_debug: boolean = false;
 
 function log(msg: string) {
     if (log_debug) {
@@ -40,13 +39,13 @@ export function send(msg: string) {
             udp.close();
         }
 
-        log(`<< ${esp_ipaddr}:${esp_udpport} (${bytes}bytes) ${msg}`);
+        log(`<< ${esp_ipaddr}:${esp_udpport} (${bytes} bytes, ${bytes/16} words) ${msg}`);
     });
 }
 
 // Start the UDP client by defining event listerners and binding the socket
 // to the port, the callback is called with the message as a parameter.
-export function start(callback: (message: string) => void) {
+export function start(callback: (msg: Buffer) => void) {
     if (esp_udpport == -1 || esp_ipaddr == "") {
         console.error("No UDP port or IP address specified, run `init` first!");
         return;
@@ -58,7 +57,18 @@ export function start(callback: (message: string) => void) {
     });
 
     udp.on("message", (msg, rinfo) => {
-        log(`>> ${rinfo.address}:${rinfo.port} ${msg.toString()}`);
+        let length = msg.length;
+        // let msg_short = "";
+
+        // // decompose the message into words.
+        // msg_short = msg.slice(0, 16).toString('hex');
+        // msg_short += " ";
+        // msg_short += msg.slice(16, 32).toString('hex');
+        // msg_short += " ... ";
+        // msg_short += msg.slice(-16).toString('hex');
+        // console.log(`ciphertext: ${msg_short}`);
+
+        log(`>> ${rinfo.address}:${rinfo.port} (${length} bytes, ${length / 16} words)`);
         callback(msg);
     });
 
