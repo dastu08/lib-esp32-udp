e80f85638fd14feb6eb719b8ae38c665e369d41d
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-09-05 18:18:34 +0200

    Correct padding of messages.
    Padd messages with zeros to a length of 256 bytes.
    
    Added logging function to hide the output similar as in udp-client.

--- aes-256-cbc.ts
+++ aes-256-cbc.ts
@@ -1,13 +1,30 @@
 import * as crypto from "crypto";
 
 const algo = "aes-256-cbc";
+const buffer_length = 256;
+
+var log_debug: boolean = false;
+
+function log(msg: string) {
+    if (log_debug) {
+        console.log(`[aes-256-cbc] ${msg}`);
+    }
+}
+
+export function loggingEnable() {
+    log_debug = true;
+}
+
+export function loggingDisable() {
+    log_debug = false;
+}
 
 // generate a 256 bit key (32 bytes)
 export function generateKey(): Buffer {
     // First, we'll generate the key. The key length is dependent on the algorithm.
     // In this case for aes256, it is 32 bytes (256 bits).
     let key: Buffer = crypto.randomFillSync(Buffer.alloc(32));
-    console.log("Generated 256bit key");
+    log("Generated 256bit key");
     return key;
 }
 
@@ -15,7 +32,7 @@ export function generateKey(): Buffer {
 export function generateIv(): Buffer {
     // aes block size is always 16 bytes
     const iv: Buffer = crypto.randomFillSync(Buffer.alloc(16));
-    console.log('Generated iv');
+    log('Generated iv');
     return iv;
 }
 
@@ -23,10 +40,21 @@ export function generateIv(): Buffer {
 export function encrypt(key: Buffer, plainText: string, callback: (cipherText: Buffer) => any) {
     // generate IV for this message
     let iv: Buffer = generateIv();
-    
+    let buffer_in: Buffer = Buffer.alloc(buffer_length, 0);
+    let buffer_plaintext = Buffer.from(plainText, 'utf-8');
+    let plain_len = plainText.length
+
+    if (plain_len > buffer_length) {
+        log(`Cannot encrypt a message of length ${plain_len}, maximum length is ${buffer_length}. Aborting`)
+        return;
+    }
+    for (let i = 0; i < plainText.length; i++) {
+        buffer_in[i] = buffer_plaintext[i];
+    }
+
     // cipher can be now used as a stream
     const cipher = crypto.createCipheriv(algo, key, iv);
-    // cipher.setAutoPadding(false);
+    cipher.setAutoPadding(false);
 
     let cipherText: Buffer = iv;
 
@@ -38,11 +66,11 @@ export function encrypt(key: Buffer, plainText: string, callback: (cipherText: B
         cipherText = Buffer.concat([cipherText, chunk]);
     });
     cipher.on("end", () => {
-        console.log("Encryption finished.");
+        log("Encryption finished.");
         callback(cipherText);
     });
 
-    cipher.write(plainText, 'utf8');
+    cipher.write(buffer_in);
     cipher.end();
 }
 
@@ -69,7 +97,7 @@ export function decrypt(key: Buffer, cipherText: Buffer, callback: (plainText: s
         plainText = Buffer.concat([plainText, chunk]);
     });
     decipher.on("end", () => {
-        console.log("Decryption finished.");
+        log("Decryption finished.");
         callback(plainText.toString().replace(/\0/g, ''));
     });
 
